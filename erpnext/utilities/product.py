208383431a83db02bee1063b0d7facf25ba62b69
britlog <info@britlog.com>
2018-01-23 22:04:10 +0100

    Display website stock in sales uom

--- erpnext/utilities/product.py
+++ erpnext/utilities/product.py
@@ -19,8 +19,11 @@ def get_qty_in_stock(item_code, item_warehouse_field, warehouse=None):
 		warehouse = frappe.db.get_value("Item", template_item_code, item_warehouse_field)
 
 	if warehouse:
-		stock_qty = frappe.db.sql("""select GREATEST(actual_qty - reserved_qty, 0) from tabBin where
-			item_code=%s and warehouse=%s""", (item_code, warehouse))
+		stock_qty = frappe.db.sql("""select GREATEST(S.actual_qty - S.reserved_qty, 0) / IFNULL(C.conversion_factor, 1) 
+			from tabBin S
+			inner join `tabItem` I on S.item_code = I.Item_code
+			left join `tabUOM Conversion Detail` C on I.sales_uom = C.uom and C.parent = I.Item_code 
+			where S.item_code=%s and S.warehouse=%s""", (item_code, warehouse))
 
 	if stock_qty:
 		stock_qty = adjust_qty_for_expired_items(item_code, stock_qty, warehouse)
@@ -35,7 +38,7 @@ def adjust_qty_for_expired_items(item_code, stock_qty, warehouse):
 	batches = frappe.get_all('Batch', filters=[{'item': item_code}], fields=['expiry_date', 'name'])
 	expired_batches = get_expired_batches(batches)
 	stock_qty = [list(item) for item in stock_qty]
-	
+
 	for batch in expired_batches:
 		if warehouse:
 			stock_qty[0][0] = max(0, stock_qty[0][0] - get_batch_qty(batch, warehouse))
